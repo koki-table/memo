e3cd33b48a19bb7f9eead3126422dceaa58cf824
koki-shino <shino.k@x-point-1.net>
2023-03-12 17:48:44 +0900

    feat: ✨  layout追加

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,19 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Outlet } from 'react-router-dom'
 
 import { NotFound } from '@/features/misc'
+import { MainLayout } from '@/Layout'
+import { Suspense } from '@/providers/Suspense'
 import { lazyImport } from '@/utils/lazyImport'
+
+const ProtectedLayout = () => {
+  return (
+    <MainLayout>
+      <Suspense>
+        <Outlet />
+      </Suspense>
+    </MainLayout>
+  )
+}
 
 const { AuthRoutes } = lazyImport(async () => await import('@/features/auth'), 'AuthRoutes')
 const { App } = lazyImport(async () => await import('@/App'), 'App')
@@ -9,7 +21,7 @@ const { App } = lazyImport(async () => await import('@/App'), 'App')
 export const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/">
+      <Route path="/" element={<ProtectedLayout />}>
         <Route index element={<App />} />
       </Route>
       <Route path="/auth/*" element={<AuthRoutes />} />
