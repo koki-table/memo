502e518a731088a58911b7a79a9960b1afe5b355
koki-shino <shino.k@x-point-1.net>
2023-04-20 22:39:46 +0900

    feat: ✨  form大枠追加

--- src/features/note/components/NoteComponent.tsx
+++ src/features/note/components/NoteComponent.tsx
@@ -1,8 +1,10 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import { Text, HStack, VStack, Box } from '@chakra-ui/react'
+import { Text, chakra, VStack, Box } from '@chakra-ui/react'
 import { FC } from 'react'
+import { useForm } from 'react-hook-form'
 import { useParams } from 'react-router-dom'
 
+import { Button } from '@/components/Elements'
 import { ImgInput } from '@/components/Form/ImgInput'
 import { Textarea } from '@/components/Form/Textarea'
 
@@ -16,6 +18,14 @@ export const NoteComponent: FC = () => {
 
   const formattedDate = `${id!.slice(0, 4)}/${id!.slice(4, 6)}/${id!.slice(6)}`
 
+  const onSubmit = (data: any) => console.log(data)
+
+  const {
+    register,
+    handleSubmit,
+    // formState: { errors },
+  } = useForm()
+
   return (
     <VStack
       px={'4'}
@@ -26,15 +36,23 @@ export const NoteComponent: FC = () => {
       maxW={['100%', '400px']}
       margin="0 auto"
       minH={`calc(100vh)`}
-      spacing={6}
     >
-      <Text w={'100%'} fontSize={'sm'} fontWeight="700">
-        {formattedDate}
-      </Text>
-      <ImgInput />
-      <Box minW={'100%'}>
-        <Textarea placeholder="メモしたいことを記載" minH={'180px'} />
-      </Box>
+      <chakra.form onSubmit={handleSubmit(onSubmit)}>
+        <VStack spacing={6}>
+          <Text w={'100%'} fontSize={'sm'} fontWeight="700">
+            {formattedDate}
+          </Text>
+          <ImgInput />
+          <Box minW={'100%'}>
+            <Textarea placeholder="メモしたいことを記載" minH={'180px'} {...register('example')} />
+          </Box>
+          <Button type={'submit'}>
+            <Text fontSize={'sm'} fontWeight="700">
+              保存
+            </Text>
+          </Button>
+        </VStack>
+      </chakra.form>
     </VStack>
   )
 }
