c5580be31f29f0b162efcbd65158b3190c63d140
koki-table <koki.s0327@icloud.com>
2023-06-05 23:57:23 +0900

    feat: ✨  homeのLinkItemコンポーネント追加

--- src/features/home/components/HomeComponent.tsx
+++ src/features/home/components/HomeComponent.tsx
@@ -7,6 +7,8 @@ import { useNavigate } from 'react-router-dom'
 
 import gyoza from '@/assets/gyoza.png'
 
+import { LinkItem } from './LinkItem'
+
 const bgLoop = keyframes`
   from { background-position: 0 0; }
 to { background-position: -6300px 0; }
@@ -19,43 +21,27 @@ export const HomeComponent: FC = () => {
   return (
     <VStack
       px={'4'}
-      pb={8}
-      display="flex"
       justifyContent="center"
       alignItems="center"
       maxW={['100%', '400px']}
       margin="0 auto"
       minH={`calc(100vh)`}
+      spacing={10}
     >
       <VStack>
-        <Link onClick={() => navigate(`/calendar/`)}>
-          <HStack
-            width={viewWidth}
-            alignItems={'flex-start'}
-            spacing={2}
-            pt="6"
-            pb="6"
-            borderBottom={'1px'}
-            borderColor={'var(--line-color-main)'}
-          >
-            <Text fontSize={'md'} pl={2} w={'100%'} lineHeight="1.6" fontWeight={'semibold'}>
-              Calendar
-            </Text>
-            <Box pr={3}>
-              <BsArrowRightShort size={23} />
-            </Box>
-          </HStack>
-        </Link>
-        <Box
-          backgroundImage={gyoza}
-          w={'120%'}
-          h={'50px'}
-          backgroundPosition="0 0"
-          backgroundSize={'auto 100%'}
-          backgroundRepeat={'repeat-x'}
-          animation={`${bgLoop} 380s linear infinite`}
-        />
+        <LinkItem text={'カレンダー'} path={'/calendar'} />
+        <LinkItem text={'レシピリスト'} path={'/recipe/list'} />
       </VStack>
+      <Box
+        backgroundImage={gyoza}
+        w={'120%'}
+        h={'50px'}
+        backgroundPosition="0 0"
+        backgroundSize={'auto 100%'}
+        backgroundRepeat={'repeat-x'}
+        animation={`${bgLoop} 380s linear infinite`}
+        mt={10}
+      />
     </VStack>
   )
 }
