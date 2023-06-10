9e7fffb2803014b63835f768afdcbb33d455cb96
koki-table <koki.s0327@icloud.com>
2023-06-10 22:17:35 +0900

    style: 🌈  文言修正

--- src/features/recipe/components/RecipeListComponent.tsx
+++ src/features/recipe/components/RecipeListComponent.tsx
@@ -43,7 +43,7 @@ export const RecipeListComponent: FC = () => {
     >
       <VStack>
         <Heading w="100%" pb="8">
-          レシピリスト 🥘
+          料理リスト 🥘
         </Heading>
         <CategoryListComponent />
         {/* currentPageが1から始まる為、-1している */}
