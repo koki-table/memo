daf26012d58eb9856eaaeb8576ac95b07cb92ae0
koki-table <koki.s0327@icloud.com>
2023-06-10 19:25:23 +0900

    refactor: 🦄  home画面のリファクタリング

--- src/features/home/components/HomeComponent.tsx
+++ src/features/home/components/HomeComponent.tsx
@@ -1,23 +1,14 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { Text, VStack, Box, Link, keyframes, HStack } from '@chakra-ui/react'
+import { VStack } from '@chakra-ui/react'
 import { FC } from 'react'
-import { BsArrowRightShort, BsArrowLeftShort } from 'react-icons/bs'
-import { useNavigate } from 'react-router-dom'
 
 import gyoza from '@/assets/gyoza.png'
+import { LoopingBackground } from '@/components/Elements/Animation/Loop'
 
 import { LinkItem } from './LinkItem'
 
-const bgLoop = keyframes`
-  from { background-position: 0 0; }
-to { background-position: -6300px 0; }
-`
-
 export const HomeComponent: FC = () => {
-  const navigate = useNavigate()
-  const viewWidth = window.innerWidth - 32
-
   return (
     <VStack
       px={'4'}
@@ -32,16 +23,7 @@ export const HomeComponent: FC = () => {
         <LinkItem text={'カレンダー'} path={'/calendar'} />
         <LinkItem text={'レシピリスト'} path={'/recipe/list'} />
       </VStack>
-      <Box
-        backgroundImage={gyoza}
-        w={'120%'}
-        h={'50px'}
-        backgroundPosition="0 0"
-        backgroundSize={'auto 100%'}
-        backgroundRepeat={'repeat-x'}
-        animation={`${bgLoop} 380s linear infinite`}
-        mt={10}
-      />
+      <LoopingBackground source={gyoza} w="120%" h={'50px'} animationDuration={'380s'} />
     </VStack>
   )
 }
